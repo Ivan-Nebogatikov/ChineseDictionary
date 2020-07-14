6be02168bcbdf429a05e4aa4207fb79f68a14817
Анастасия Владимировна Смирнова <avla.smirnova@live.unecon.ru>
2020-07-14 14:47:54 +0300

    Added examples.

--- ChineseDictionary/ChineseDictionary/Services/MockDictionaryService.cs
+++ ChineseDictionary/ChineseDictionary/Services/MockDictionaryService.cs
@@ -18,7 +18,10 @@ namespace ChineseDictionary.Services
         {
             mockDict = new List<ExtendedWord>
             {
-                new ExtendedWord{ Chinese = "你好", Pinyin = "nǐ hǎo", Translations = new List<string>{ "привет", "здравствуй" }, Examples = new List<Example> { new Example { Chinese = "王女士，你好！", Translation = "Добрый день, госпожа Ван!" } } },
+                new ExtendedWord{ 
+                    Chinese = "你好", Pinyin = "nǐ hǎo", Translations = new List<string>{ "привет", "здравствуй" }, 
+                    Examples = new List<Example> { new Example { Chinese = "王女士，你好！", Translation = "Добрый день, госпожа Ван!" } } , 
+                    },
                 new ExtendedWord{ Chinese = "你", Pinyin = "nǐ", Translations = new List<string>{ "ты", "твой" } },
                 new ExtendedWord{ Chinese = "好", Pinyin = "hǎo", Translations = new List<string>{ "хорошо" } }
             };
@@ -37,6 +40,15 @@ namespace ChineseDictionary.Services
         public IEnumerable<ExtendedWord> SearchByTranslation(string translation, int skip = 0, int take = int.MaxValue)
         {
             return mockDict.Where(x => x.Translations.Any(y => y.Contains(translation))).Skip(0).Take(take);
+        }
+
+        public ExtendedWord GetByChinese(string chinese)
+        {
+            // ToDo: возможны проблемы со скоростью поиска
+            foreach (ExtendedWord word in mockDict)
+                if (word.Chinese == chinese)
+                    return word;
+            return new ExtendedWord() { Chinese = "error" };
         }
     }
 }
