e9b341f5716bd3d105852285f199d7f1a2e13b8a
Ivan-Nebogatikov <ivan.nebogatikov@gmail.com>
2020-07-15 10:24:19 +0500

    added console project

---
+++ Handwriting/Handwriting/Program.cs
@@ -0,0 +1,20 @@
+﻿using HandwritingLibrary;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Handwriting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileData = File.ReadAllText("也.txt");
+            var rawStrokes = JsonConvert.DeserializeObject<List<List<List<double>>>>(fileData);
+            var strokes = RawStokeConverter.ParseData(rawStrokes);
+            var analyzer = new Analyzer(strokes);
+            var subStrokes = analyzer.AnalyzedStrokes;
+        }
+    }
+}
---
+++ Handwriting/HandwritingLibrary/RawStokeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HandwritingLibrary
+{
+    public class RawStokeConverter
+    {
+        public static List<Stroke> ParseData(List<List<List<double>>> rawData)
+        {
+            var res = new List<Stroke>();
+            foreach(var stroke in rawData)
+            {
+                var resStroke = new Stroke();
+                resStroke.Points = stroke.Select(x => new Point { X = x[0], Y = x[1] }).ToList();
+                if (resStroke.Points.Any())
+                {
+                    res.Add(resStroke);
+                }
+            }
+            return res;
+        }
+    }
+}
