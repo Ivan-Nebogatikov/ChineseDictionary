a0c83d57e624907c5de2f46994b48c4a24f8f93b
Анастасия Владимировна Смирнова <avla.smirnova@live.unecon.ru>
2020-07-22 00:04:13 +0300

    Test of stroke order

---
+++ ChineseDictionary/ChineseDictionary/wwwroot/strokeOrder.js
@@ -0,0 +1,14 @@
+﻿function characterDraw() {
+	var writer = HanziWriter.create('character-target-div', '国', {
+		width: 100,
+		height: 100,
+		padding: 5,
+		showOutline: true
+	});
+	document.getElementById('animate-button').addEventListener('click', function () {
+		writer.animateCharacter();
+	});
+} 
+
+
+
