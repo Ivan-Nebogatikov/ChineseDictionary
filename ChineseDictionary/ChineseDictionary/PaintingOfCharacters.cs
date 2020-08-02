df3387335597d576f930f555ed74e24f2c1fae44
Анастасия Владимировна Смирнова <avla.smirnova@live.unecon.ru>
2020-08-02 14:29:09 +0300

    Done changes from comments

--- ChineseDictionary/ChineseDictionary/PaintingOfCharacters.cs
+++ ChineseDictionary/ChineseDictionary/PaintingOfCharacters.cs
@@ -3,60 +3,43 @@ using ChineseDictionary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ChineseDictionary
 {
     public class PaintingOfCharacters
     {
+        public static char[] firstTone = new char[] { 'Ā', 'ā', 'Ē', 'ē', 'Ī', 'ī', 'Ō', 'ō', 'Ū', 'ū', 'Ǖ', 'ǖ' };
+        public static char[] secondTone = new char[] { 'Á', 'á', 'É', 'é', 'Í', 'í', 'Ó', 'ó', 'Ú', 'ú', 'Ǘ', 'ǘ' };
+        public static char[] thirdTone = new char[] { 'Ǎ', 'ǎ', 'Ě', 'ě', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǚ', 'ǚ' };
+        public static char[] fourthTone = new char[] { 'À', 'à', 'È', 'è', 'Ì', 'ì', 'Ò', 'ò', 'Ù', 'ù', 'Ǜ', 'ǜ' };
+        public static char[] fifthTone = new char[] { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Ü', 'ü' };
+        public static char[] separators = new char[] { 'b', 'p', 'm', 'f', 'd', 't', 'n', 'l', 'g', 'k', 'h', 'j', 'q', 'x', 'z', 'c', 's', 'r', 'y' };
+
         public static List<ColoredCharacter> PaintCharacter(string word, string chinese)
         {
-            char[] firstTone = new char[] { 'Ā', 'ā', 'Ē', 'ē', 'Ī', 'ī', 'Ō', 'ō', 'Ū', 'ū', 'Ǖ', 'ǖ' };
-            char[] secondTone = new char[] { 'Á', 'á', 'É', 'é', 'Í', 'í', 'Ó', 'ó', 'Ú', 'ú', 'Ǘ', 'ǘ' };
-            char[] thirdTone = new char[] { 'Ǎ', 'ǎ', 'Ě', 'ě', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǚ', 'ǚ' };
-            char[] fourthTone = new char[] { 'À', 'à', 'È', 'è', 'Ì', 'ì', 'Ò', 'ò', 'Ù', 'ù', 'Ǜ', 'ǜ' };
-            char[] fifthTone = new char[] { 'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Ü', 'ü' };
+            string[] split = word.Split(separators);
 
-            string[] split = word.Split(new Char[] { 'b', 'p', 'm', 'f', 'd', 't', 'n', 'l', 'g', 'k', 'h', 'j', 'q', 'x', 'z', 'c', 's', 'r', 'y' });
-
-            int i;
             int j = 0;
-            string style = "";
+            string style = ColorConstants.fifthTone;
             List<ColoredCharacter> coloredCharactersList = new List<ColoredCharacter> { };
 
             foreach (string splitSimbols in split)
             {
                 if (splitSimbols.Trim() != "")
                 {
-                    i = 0;
                     foreach (char simbol in splitSimbols)
                     {
                         if (firstTone.Contains(simbol))
-                            i = 1;
-                        if (secondTone.Contains(simbol))
-                            i = 2;
-                        if (thirdTone.Contains(simbol))
-                            i = 3;
-                        if (fourthTone.Contains(simbol))
-                            i = 4;
-                    }
-                    switch (i)
-                    {
-                        case 1:
                             style = ColorConstants.firstTone;
-                            break;
-                        case 2:
+                        if (secondTone.Contains(simbol))
                             style = ColorConstants.secondTone;
-                            break;
-                        case 3:
+                        if (thirdTone.Contains(simbol))
                             style = ColorConstants.thirdTone;
-                            break;
-                        case 4:
+                        if (fourthTone.Contains(simbol))
                             style = ColorConstants.fourthTone;
-                            break;
-                        case 0:
-                            style = ColorConstants.fifthTone;
-                            break;
                     }
+
                     ColoredCharacter coloredCharacter = new ColoredCharacter(chinese[j], style);
                     coloredCharactersList.Add(coloredCharacter);
                     j++;
