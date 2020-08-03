fa79d9d47266bf14c23447236e8acb617cebc52d
Old1906 <Oleggelo86@gmail.com>
2020-08-03 16:36:17 +0300

    Add some storage to search

---
+++ ChineseDictionary/ChineseDictionary/Constants/ConfigConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChineseDictionary.Constants
+{
+    public class ConfigConstants
+    {
+        // Bad names
+        public const int FirstShownWordsCount = 20;
+        public const int AddShownWordsCount = 20;
+        public const int StorageWordsSize = 200;
+    }
+}
