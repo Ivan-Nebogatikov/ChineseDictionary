aa51827cbbf6371bd1e7a4d133f5ab2e95285a77
ekaterinatretyak <45742265+ekaterinatretyak@users.noreply.github.com>
2020-08-08 23:43:40 +0400

    Update CharConstants.cs

--- Handwriting/HandwritingLibrary/CharConstants.cs
+++ Handwriting/HandwritingLibrary/CharConstants.cs
@@ -2,7 +2,6 @@
 {
     public class CharConstants
     {
-        
         public const int MAX_CHARACTER_STROKE_COUNT = 48;
         public const int MAX_CHARACTER_SUB_STROKE_COUNT = 64;
         public const double DEFAULT_LOOSENESS = 0.15;
@@ -10,38 +9,5 @@
         public const double SKIP_PENALTY_MULTIPLIER = 1.75; 
         public static double CORRECT_NUM_STROKES_BONUS = 0.1; 
         public static int CORRECT_NUM_STROKES_CAP = 10; 
-
-        public char Character;
-
-        public int StrokeCount;	
-        public int SubStrokeCount;
-
-        private int[] directions = new int[MAX_CHARACTER_SUB_STROKE_COUNT];
-        private double[] lengths = new double[MAX_CHARACTER_SUB_STROKE_COUNT];
-        private double[] centers = new double[MAX_CHARACTER_STROKE_COUNT];
-
-        public int[] Directions
-        {
-            get
-            {
-                return directions;
-            }
-        }
-
-        public double[] Lengths
-        {
-            get
-            {
-                return lengths;
-            }
-        }
-
-        public double[] Centers
-        {
-            get
-            {
-                return centers;
-            }
-        }
     }
 }
